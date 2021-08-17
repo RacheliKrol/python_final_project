fee99a3bce7f001aa8e36bc66e46a4d4cc9b339f
RacheliKrol <r0527120682@gmail.com>
2021-08-17 14:42:08 +0300

    divide to classes

--- ‏‏finalProject/app.py
+++ ‏‏finalProject/app.py
@@ -1,31 +1,38 @@
 from flask import Flask, request, render_template
 import backend_code
+class Application:
+    def __init__(self):
+        pass
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
-
-@app.route('/')# loud the form to fill the properties
-def load():
-    return render_template('home.html')
-
-@app.route('/replace-link/', methods=['POST', 'GET'])
-def replace(): #active the function- rename_file when the user submit the properties
-    if request.method == 'POST':
-        oldName = request.form['oldName']
-        newName = request.form['newName']
-        path = request.form['path']
-        backend_code.renameFile(path, oldName, newName)
-        return render_template('success.html')
-    return render_template('home.html')
-
-@app.route("/cancel-last-replace/", methods=['POST', 'GET'])
-def cancel_last_replace(): #active the function- cancel_last_action when the user click on the button
-    if request.method == 'POST':
-        backend_code.cancel_last_action()
+    @app.route('/')# loud the form to fill the properties
+    def load(self):
         return render_template('home.html')
-    return render_template('success.html')
+
+    @app.route('/replace-link/', methods=['POST', 'GET'])
+    def replace(self): #active the function- rename_file when the user submit the properties
+        if request.method == 'POST':
+            oldName = request.form['oldName']
+            newName = request.form['newName']
+            path = request.form['path']
+            backend_code.renameFile(path, oldName, newName)
+            return render_template('success.html')
+        return render_template('home.html')
+
+    @app.route("/cancel-last-replace/", methods=['POST', 'GET'])
+    def cancel_last_replace(self): #active the function- cancel_last_action when the user click on the button
+        if request.method == 'POST':
+            backend_code.cancel_last_action()
+            return render_template('home.html')
+        return render_template('success.html')
+
+    @staticmethod
+    def run_application():
+        app.debug= True
+        app.run()
 
 
+app = Application()
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run_application()
